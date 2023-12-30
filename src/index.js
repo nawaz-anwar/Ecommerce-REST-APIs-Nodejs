7c1774d44fb4f131084eaf274430f84545fc7005
Nawaz <nzan576@gmail.com>
2023-12-30 18:30:39 +0530

    User and Auth Routes Implemented

--- src/index.js
+++ src/index.js
@@ -10,4 +10,10 @@ app.get("/", (res,req)=>{
     return res.status(200).send({message : "welcome to ecommerce api - node", status:true})
 })
 
+const authRouters = require("./routes/auth.route.js");
+app.use("/auth",authRouters);
+
+const userRouters = require("./routes/user.route.js");
+app.use("/users",userRouters);
+
 module.exports = app;
